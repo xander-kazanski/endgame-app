02cc990b4bee3597e3d651aeff8cf71bceb87dd2
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-12-05 09:27:57 -0500

    adding the languages

--- App.jsx
+++ App.jsx
@@ -1,14 +1,10 @@
 import React from "react"
+import { languages } from "./languages"
 
-/**
- * Goal: Build out the main parts of our app
- * 
- * Challenge: Build a status section below the header.
- * For now, you can just hard-code in the styles for
- * a winning game, and we'll make it more dynamic
- * later.
- */
-
+function Color({name, color, backgroundColor}) {
+  const styles = {color, backgroundColor}
+  return <p className="color" style={styles}>{name}</p>
+}
 export default function AssemblyEndgame() {
     return (
         <main>
@@ -16,6 +12,15 @@ export default function AssemblyEndgame() {
                 <h1>Assembly: Endgame</h1>
                 <p>Guess the word within 8 attempts to keep the 
                 programming world safe from Assembly!</p>
+                <div className="win">
+                  <p className="title">You win!</p>
+                  <p className="message">Well done! 🎉</p>
+                </div>
+                <div className="languages">
+                  {languages.map(({name, backgroundColor, color}) => {
+                    return <Color key={name} name={name} backgroundColor={backgroundColor} color={color}/>
+                  })}
+                </div>
             </header>
         </main>
     )
