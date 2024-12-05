7784d1d5451b2121fd80647258ae8f34b0a6f2a1
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-12-05 14:31:55 -0500

    adding more functonality.

--- App.jsx
+++ App.jsx
@@ -1,21 +1,56 @@
 import React, { useState } from "react"
 import { languages } from "./languages"
+import clsx from "clsx"
 
-function Language({ name, color, backgroundColor }) {
+function Language({ name, color, backgroundColor, wrongCount, idx }) {
   const styles = { color, backgroundColor }
-  return <p className="language" style={styles}>{name}</p>
+  const isLost = wrongCount > 0 && idx < wrongCount;
+  console.log(wrongCount, idx, isLost)
+  return (
+    <p className={`language ${isLost === true && 'lost'}`}  style={styles}>{name}</p>
+  )
 }
-function Letter({ letter }) {
-  return <p className="letter">{letter.toUpperCase()}</p>
+function Letter({ letter, show }) {
+  return <p className="letter">{show === true && letter.toUpperCase()}</p>
 }
-function Key({ letter }) {
-  const styles = { backgroundColor: "#FCBA29" }
-  return <button onClick={() => console.log("clicked")} style={styles} className="key">{letter.toUpperCase()}</button>
+function Key({ letter, onClick, keyClass }) {
+  const selectionClass = keyClass(letter);
+
+  return (
+    <button
+      onClick={() => onClick(letter)}
+      className={`key ${selectionClass}`}>
+      {letter.toUpperCase()}
+    </button>
+  )
 }
 export default function AssemblyEndgame() {
-  const [currentWord, setCurrentWord] = useState("Bucerotidae");
+  const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState([]);
 
+  const wrongCount = guessedLetters.filter(letter => {
+    return !currentWord.includes(letter);
+  }).length
+  console.log(wrongCount)
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+  function addGuessedLetter(letter) {
+    if (!guessedLetters.includes(letter)) {
+      setGuessedLetters(prevLetters => [...prevLetters, letter])
+    }
+  }
+
+  function getKeyClass(letter) {
+    if (currentWord.includes(letter) && guessedLetters.includes(letter)) {
+      return clsx({ right: true })
+    }
+    if (guessedLetters.includes(letter)) {
+      return clsx({ wrong: true })
+    }
+    return clsx({ base: true })
+  }
+
+
   return (
     <main>
       <header>
@@ -28,19 +63,20 @@ export default function AssemblyEndgame() {
         <p className="message">Well done! 🎉</p>
       </div>
       <div className="languages">
-        {languages.map(({ name, backgroundColor, color }) => {
-          return <Language key={name} name={name} backgroundColor={backgroundColor} color={color} />
-          })}
+        {languages.map(({ name, backgroundColor, color }, idx) => {
+          
+          return <Language key={name} name={name} wrongCount={wrongCount} idx={idx} backgroundColor={backgroundColor} color={color} />
+        })}
       </div>
       <div className="letters">
         {currentWord.split("").map((letter, idx) => {
-          return <Letter key={letter + idx} letter={letter} />
-          })}
+          return <Letter key={letter + idx} idx={idx} letter={letter} show={guessedLetters.includes(letter)} />
+        })}
       </div>
       <div className="keyboard">
         {alphabet.split("").map((letter, idx) => {
-          return <Key key={letter + idx} letter={letter} />
-          })}
+          return <Key key={letter + idx} letter={letter} onClick={addGuessedLetter} keyClass={getKeyClass} />
+        })}
       </div>
       <button type="button" className="new-game">New Game</button>
     </main>
